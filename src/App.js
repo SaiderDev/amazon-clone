b9bb69f074b5608fd93b627c71a16e2639046f5b
Sandisiwe Nkomba <gqibelankomba@gmail.com>
2024-10-26 16:29:48 +0200

    The skeleton build of the Amazon clone app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import {Route} from 'react-router-dom'
+import Home from './Components/Home'
+import Products from './Components/Products'
+import Header from './Components/layout/Header'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+
+        <main>
+            <Route path='/home'>
+              <Home />
+            </Route>
+
+            <Route path="/products">
+              <Products/>
+            </Route>
+            
+        </main>
+
     </div>
   );
 }
---
+++ src/Components/layout/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <nav>
+            <ul>
+                <li>
+                    <a href="/home">Home</a>
+                </li>
+                <li>
+                    <a href="/products">Products</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
